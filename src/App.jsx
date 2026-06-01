2103841464699afc6102f829600edc0bd0ec25d8
JayX97 <jason.fernandezx@yahoo.com>
2026-06-01 17:13:51 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './Home';
+import './App.css';
+
+function App() {
+  return (
+    <div id='app'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div id='welcome-section'>
+      <h1>Anime Max</h1>
+      <h3>Welcome to Anime Max! Please enter name of show or movie to access our vast library.</h3>
+      <input type="'text"></input>
+      <button>Search</button>
+    </div>
+  )
+}
+
+export default Home;
